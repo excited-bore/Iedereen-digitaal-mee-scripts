c7c949226d716f981012bffc88981e9d8b4e5931
excited-bore <serve-churn-spiral@duck.com>
2025-11-20 00:37:56 +0100

    Small tweaks

--- scripts/install_apps.ps1
+++ scripts/install_apps.ps1
@@ -1,31 +1,36 @@
-where winget >null 2>&1
-
-if ( $? ){
+if (Get-Command "winget" -ErrorAction SilentlyContinue){
+    Write-Host "Checking if Adobe Acrobat Reader is installed" 
     if (-Not (winget list -q Adobe.Acrobat.Reader.64-bit)){ 
         Write-Host "Installing Adobe Acrobat Reader"
         winget install --id Adobe.Acrobat.Reader.64-bit --accept-package-agreements --accept-source-agreements
     }
+   
+    Write-Host "Checking if VLC is installed" 
     if (-Not (winget list -q VideoLAN.VLC)){ 
         Write-Host "Installing VLC"
         winget install --id VideoLAN.VLC --accept-package-agreements --accept-source-agreements
     }
 
+    Write-Host "Checking if Eid is installed" 
     if (-Not (winget list -q BelgianGovernment.eIDViewer)){ 
         Write-Host "Installing Eid middleware and Eid Viewer"
         winget install --id BelgianGovernment.eIDViewer --accept-package-agreements --accept-source-agreements
         winget install --id BelgianGovernment.eIDmiddleware --accept-package-agreements --accept-source-agreements
     }
-
+    
+    Write-Host "Checking if LibreOffice is installed" 
     if (-Not (winget list -q TheDocumentFoundation.LibreOffice)){ 
-        Write-Host "Installing Libreoffice"
+        Write-Host "Installing LibreOffice"
         winget install --id TheDocumentFoundation.LibreOffice --accept-package-agreements --accept-source-agreements
     }
     
+    Write-Host "Checking if Firefox is installed" 
     if (-Not (winget list -q Mozilla.Firefox.nl)){ 
         Write-Host "Installing Firefox"
         winget install --id Mozilla.Firefox.nl --accept-package-agreements --accept-source-agreements
     }
     
+    Write-Host "Checking if Chrome is installed" 
     if (-Not (winget list -q Google.Chrome)){ 
         Write-Host "Installing Chrome"
         winget install --id Google.Chrome --accept-package-agreements --accept-source-agreements
