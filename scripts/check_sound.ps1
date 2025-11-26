d60b043560d7eada96d8b0d4fba132bc6f6aeb41
excited-bore <serve-churn-spiral@duck.com>
2025-11-26 03:09:28 +0100

    Small tweak check sound

--- scripts/check_sound.ps1
+++ scripts/check_sound.ps1
@@ -1,3 +1,7 @@
 Write-Host "Playing chimes.wav to check speakers"
-start c:\windows\media\chimes.wav
+Add-Type -AssemblyName presentationCore
+$mediaPlayer = New-Object system.windows.media.mediaplayer
+$mediaPlayer.open('c:\windows\media\chimes.wav')
+$mediaPlayer.Play()
+# start c:\windows\media\chimes.wav
 cmd.exe /c 'pause'
