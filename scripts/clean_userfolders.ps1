7dd2d3dc6e0e9893126d4464e71e8046ad18f82d
excited-bore <serve-churn-spiral@duck.com>
2026-02-27 02:41:27 +0000

    Added clean chrome and clean firefox

--- scripts/clean_userfolders.ps1
+++ scripts/clean_userfolders.ps1
@@ -1,26 +1,26 @@
-Write-Host "Cleaning up Contacts"
+Write-Host "Cleaning up Contacts" -ForegroundColor Cyan
 Remove-Item -Path "C:\User\$env:USERNAME\Contacts\" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "Cleaning up Desktop"
+Write-Host "Cleaning up Desktop" -ForegroundColor Cyan
 Remove-Item -Path "C:\User\$env:USERNAME\Desktop\" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "Cleaning up Downloads"
+Write-Host "Cleaning up Downloads" -ForegroundColor Cyan
 Remove-Item -Path "C:\User\$env:USERNAME\Downloads\" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "Cleaning up Documents"
+Write-Host "Cleaning up Documents" -ForegroundColor Cyan
 Remove-Item -Path "C:\User\$env:USERNAME\Documents\" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "Cleaning up Onedrive"
+Write-Host "Cleaning up Onedrive" -ForegroundColor Cyan 
 Remove-Item -Path "C:\User\$env:USERNAME\Onedrive\" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "Cleaning up Pictures"
+Write-Host "Cleaning up Pictures" -ForegroundColor Cyan
 Remove-Item -Path "C:\User\$env:USERNAME\Pictures\" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "Cleaning up Saved Games"
+Write-Host "Cleaning up Saved Games" -ForegroundColor Cyan
 Remove-Item -Path "C:\User\$env:USERNAME\Saved Games\" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "Cleaning up Searches"
+Write-Host "Cleaning up Searches" -ForegroundColor Cyan
 Remove-Item -Path "C:\User\$env:USERNAME\Searches\" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "Cleaning up Videos"
+Write-Host "Cleaning up Videos" -ForegroundColor Cyan
 Remove-Item -Path "C:\User\$env:USERNAME\Videos\" -Recurse -Force -ErrorAction SilentlyContinue
