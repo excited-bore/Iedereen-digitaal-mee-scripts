78afdd5271d0bc9f4c5bf2ceb5041c56e20df97a
excited-bore <serve-churn-spiral@duck.com>
2026-03-14 19:31:17 +0100

    Hpupdates now doesn't check for winget first

--- scripts/hpupdates.ps1
+++ scripts/hpupdates.ps1
@@ -1,6 +1,4 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\check_winget.ps1"
-
-if ((winget list -q HP.ImageAssistant) -eq 'No installed package found matching input criteria.'){
+if ((winget list -q HP.ImageAssistant --accept-source-agreements) -eq 'No installed package found matching input criteria.'){
     winget install --id HP.ImageAssistant --silent --accept-package-agreements --accept-source-agreements
 }
 
