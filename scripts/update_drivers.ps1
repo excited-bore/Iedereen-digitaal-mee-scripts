a7ec2644cae5ac34ed8b7fed57df2eec590b23a4
excited-bore <serve-churn-spiral@duck.com>
2026-03-18 03:09:06 +0100

    Small tweak to update-driver. First installing and running updater, then install supportassist

--- scripts/update_drivers.ps1
+++ scripts/update_drivers.ps1
@@ -9,14 +9,12 @@ if ( $manufacturer -eq 'Dell Inc.'){
     Write-Host "Dell System detected." -ForegroundColor Cyan
 
     # Check if Dell Commandline is installed; if not, install 
-
     
     Write-Host "Installing and running Dell Commandline for updates..." -ForegroundColor Yellow
 
     & "$PSScriptRoot\dellupdate.ps1"
 
     # Same for Dell Supportassist
-	
 	
     Write-Host "Installing Dell SupportAssist..." -ForegroundColor Yellow
 
@@ -27,29 +25,29 @@ if ( $manufacturer -eq 'Dell Inc.'){
     
     Write-Host "HP system detected." -ForegroundColor Cyan
     
-    # Check if HPsupportassist is installed; if not, install 
-
-    Write-Host "Installing HP Support Assistant..." -ForegroundColor Yellow
-
-    & "$PSScriptRoot\hpsupportassist.ps1"
-
-    # Same for HP Image assistant. This will also update
+    # Check if HP Image assistant is installed. This will also update
 
     Write-Host "Installing and running HP Image assistant for updates..." -ForegroundColor Yellow
     
     & "$PSScriptRoot\hpupdates.ps1"
+
+    # Same for HPsupportassist; if not installed, install 
+
+    Write-Host "Installing HP Support Assistant..." -ForegroundColor Yellow
+
+    & "$PSScriptRoot\hpsupportassist.ps1"
     	
 } elseif ( $manufacturer -eq 'LENOVO' ){
     
     Write-Host "Lenovo system detected." -ForegroundColor Cyan
     
-    Write-Host "Installing Lenovo Vantage..." -ForegroundColor Yellow
-
-    & "$PSScriptRoot\lenovovantage.ps1"
-    
     Write-Host "Installing Lenovo Update..." -ForegroundColor Yellow
     
     & "$PSScriptRoot\lenovoupdate.ps1"
+    
+    Write-Host "Installing Lenovo Vantage..." -ForegroundColor Yellow
+
+    & "$PSScriptRoot\lenovovantage.ps1"
 
 } elseif ( $manufacturer -eq "ASUSTeK COMPUTER INC." ) {
     Write-Host "This is an ASUS system."
